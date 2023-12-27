acbef88df2df461cc94a6617e9623da749db170b
bongcrew <bongcrew1@naver.com>
2023-12-27 18:07:13 +0900

    12/27작업

--- js/script.js
+++ js/script.js
@@ -3,23 +3,24 @@ $(function () {
   $('#fullpage').fullpage({
     // 1. 앵커 설정
     anchors: ['home', 'about', 'work', 'contact'],
+
     // navigation: true,
 
     // 2. menu 적용(인디케이터 커스텀)
-    /* menu: '#myMenu', */
+    menu: '#myMenu',
 
     // 3. 필요 옵션 적용
     // 스크롤 속도 : 기본값 700
     scrollingSpeed: 700,
 
     // 고정시킬 요소
-    /* fixedElements: '#header', */
+    fixedElements: '#header',
 
     // 영역 콘텐츠의 세로 정렬
-    verticalCentered: false,
+    // verticalCentered: false,
 
     // 스크롤바 생성되게
-    scrollBar: false,
+    // scrollBar: false,
 
     // 큰 영역으로 이동할 때 상단에 철컥 붙게
     bigSectionsDestination: 'top',
@@ -41,58 +42,59 @@ $(function () {
       }
     }, */
   });
-
-  // 슬라이더 시작
-  // const topSlider = new Swiper('.top-slider', {
-  //   effect: 'coverflow',
-  //   grabCursor: true,
-  //   centeredSlides: true,
-  //   slidesPerView: 'auto',
-  //   coverflowEffect: {
-  //     rotate: 50,
-  //     stretch: 0,
-  //     depth: 100,
-  //     modifier: 1,
-  //     slideShadows: true,
-  //   },
-
-  // 페이지네이션
-  // pagination: {
-  //   el: '.top-slider .swiper-pagination',
-  // },
-
-  // Navigation arrows
-  // navigation: {
-  //   nextEl: '.top-slider .swiper-button-next',
-  //   prevEl: '.top-slider .swiper-button-prev',
-  // },
-
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.top-slider .swiper-scrollbar',
-  // },
-
-  // 로딩!!시작
-  //   const $window = $(window);
-  //   const $body = $('body');
-  //   const $loading = `<div class="loading">
-  //   <div class="spinner"></div>
-  //   <div class="lds-heart"><div></div></div>
-  // </div>`;
-
-  // body 마지막 부분에 집어넣기
-  // $body.append($loading);
-  // const $target = $('.loading');
-  // 로딩이 완료되면 (Load)
-  // $window.on('load', function () {
-  // 너무 빨리 사라지므로 조금있다가 사라지게끔
-  // setTimeout(동작, 시간)
-  // setTimeout(function () {
-  //   $target.fadeOut();
-
-  // 1초후에 loading요소 지우기
-  //   setTimeout(function () {
-  //     $target.remove();
-  //   }, 1000);
-  // }, 1000);
 });
+
+// 슬라이더 시작
+// const topSlider = new Swiper('.top-slider', {
+//   effect: 'coverflow',
+//   grabCursor: true,
+//   centeredSlides: true,
+//   slidesPerView: 'auto',
+//   coverflowEffect: {
+//     rotate: 50,
+//     stretch: 0,
+//     depth: 100,
+//     modifier: 1,
+//     slideShadows: true,
+//   },
+
+// 페이지네이션
+// pagination: {
+//   el: '.top-slider .swiper-pagination',
+// },
+
+// Navigation arrows
+// navigation: {
+//   nextEl: '.top-slider .swiper-button-next',
+//   prevEl: '.top-slider .swiper-button-prev',
+// },
+
+// And if we need scrollbar
+// scrollbar: {
+//   el: '.top-slider .swiper-scrollbar',
+// },
+
+// 로딩!!시작
+//   const $window = $(window);
+//   const $body = $('body');
+//   const $loading = `<div class="loading">
+//   <div class="spinner"></div>
+//   <div class="lds-heart"><div></div></div>
+// </div>`;
+
+// body 마지막 부분에 집어넣기
+// $body.append($loading);
+// const $target = $('.loading');
+// 로딩이 완료되면 (Load)
+// $window.on('load', function () {
+// 너무 빨리 사라지므로 조금있다가 사라지게끔
+// setTimeout(동작, 시간)
+// setTimeout(function () {
+//   $target.fadeOut();
+
+// 1초후에 loading요소 지우기
+//   setTimeout(function () {
+//     $target.remove();
+//   }, 1000);
+// }, 1000);
+// });
