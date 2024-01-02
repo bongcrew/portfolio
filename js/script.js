89df2ec494b900e4cd561919ac66fe316b8c6925
bongcrew <bongcrew1@naver.com>
2024-01-02 18:08:05 +0900

    1/2작업

--- js/script.js
+++ js/script.js
@@ -1,4 +1,57 @@
 $(function () {
+  // 대상을 변수에 저장
+  const $popup = $('.popup');
+  const $btnClose = $('.btn-close');
+  const $dim = $('.dim');
+
+  $('.about-contents a').on('click', (e) => {
+    e.preventDefault();
+
+    $dim.fadeIn();
+
+    // .animate({옵션}, 지속시간, 콜백함수-끝난후의 동작)
+    $popup.animate(
+      {
+        top: '50%',
+        opacity: 1,
+      },
+      300
+      // () => {
+      //   alert('창이 나왔삼!');
+      // }
+    );
+  });
+
+  // btnClose를 클릭하면 팝업창이 닫히게
+  $btnClose.on('click', () => {
+    $popup.animate(
+      {
+        top: '-100%',
+        opacity: 0,
+      },
+      300,
+      () => {
+        $dim.fadeOut();
+      }
+    );
+  });
+  // 로딩!!시작
+  const $window = $(window);
+  const $loading = $('.loading');
+
+  // 로딩이 완료되면 (Load)
+  $window.on('load', function () {
+    // 너무 빨리 사라지므로 조금있다가 사라지게끔
+    // setTimeout(동작, 시간);
+    setTimeout(function () {
+      $loading.fadeOut();
+    }, 5000);
+  });
+  // 1초후에 loading요소 지우기
+  // setTimeout(function () {
+  //   $target.remove();
+  // }, 3000);
+
   //대상을 변수에 저장
   const $btnTop = $('.btn-top');
   const $header = $('#header');
@@ -99,28 +152,3 @@ $(function () {
     },
   });
 });
-
-// 로딩!!시작
-//   const $window = $(window);
-//   const $body = $('body');
-//   const $loading = `<div class="loading">
-//   <div class="spinner"></div>
-//   <div class="lds-heart"><div></div></div>
-// </div>`;
-
-// body 마지막 부분에 집어넣기
-// $body.append($loading);
-// const $target = $('.loading');
-// 로딩이 완료되면 (Load)
-// $window.on('load', function () {
-// 너무 빨리 사라지므로 조금있다가 사라지게끔
-// setTimeout(동작, 시간)
-// setTimeout(function () {
-//   $target.fadeOut();
-
-// 1초후에 loading요소 지우기
-//   setTimeout(function () {
-//     $target.remove();
-//   }, 1000);
-// }, 1000);
-// });
